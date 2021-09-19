aa149f6d138138a8f90d0f78e0b5f2fdac976fe4
Collins Kanyiri <collins.kanyiri@student.moringaschool.com>
2021-09-19 23:53:54 +0300

    app/config.py

--- app/config.py
+++ app/config.py
@@ -1,4 +1,39 @@
+import os
+
+
+
+
 class Config:
-    """
-    General parent configuration parent class
-    """
+    '''
+    General configuration parent class
+    '''
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    pass
+
+
+class ProdConfig(Config):
+    '''
+    Production  configuration child class
+
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+    DEBUG = False
+
+
+
+class DevConfig(Config):
+    '''
+    Development  configuration child class
+
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+
+    DEBUG = True
+
+
+config_options = {
+    'development': DevConfig,
+    'production': ProdConfig,
+}
