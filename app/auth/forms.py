849d11e64afb613d14686f4b2b357b9ff4123524
Collins Kanyiri <collins.kanyiri@student.moringaschool.com>
2021-09-27 23:54:22 +0300

    deployment to heroku

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -1,20 +1,27 @@
 from flask_wtf import FlaskForm
 from wtforms import ValidationError
-from wtforms.validators import Required, Email, EqualTo
+from wtforms.validators import DataRequired, Email, EqualTo
 from ..models import User
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
 
 
 class RegistrationForm(FlaskForm):
-    email = StringField("Your Email Address", validators=[Required(), Email()])
-    username = StringField("Enter your username", validators=[Required()])
-    password = PasswordField('Password', validators=[Required(), EqualTo(
-        'password_confirm', message="Passwords must match")])
+    email = StringField("Your Email Address", validators=[
+                       DataRequired(), Email()])
+    username = StringField("Enter your username", validators=[DataRequired()])
+    password = PasswordField(
+       "Password",
+       validators=[
+           DataRequired(),
+           EqualTo("password_confirm", message="Passwords must match"),
+       ],
+    )
     password_confirm = PasswordField(
-        "Confirm Passwords", validators=[Required()])
-    submit = SubmitField('Sign Up')
+       "Confirm Password", validators=[DataRequired()])
+    submit = SubmitField("Sign Up")
 
     def validate_email(self, data_field):
+
         if User.query.filter_by(email=data_field.data).first():
             raise ValidationError("There is an Account with that Email")
 
@@ -24,7 +31,8 @@ class RegistrationForm(FlaskForm):
 
 
 class LoginForm(FlaskForm):
-    email = StringField('Your Email Address', validators=[Required(), Email()])
-    password = PasswordField('Password', validators=[Required()])
+    email = StringField("Your Email Address", validators=[
+                         DataRequired(), Email()])
+    password = PasswordField("Password", validators=[DataRequired()])
     remember = BooleanField("Remember me")
     submit = SubmitField("Sign In")
