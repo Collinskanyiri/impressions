883bbc2d7f29ba98b3efd7606bce741ad5eb7848
Collins Kanyiri <collins.kanyiri@student.moringaschool.com>
2021-09-27 23:32:49 +0300

    deployment to heroku

--- app/__init__.py
+++ app/__init__.py
@@ -3,13 +3,13 @@ from flask_bootstrap import Bootstrap
 from config import config_options
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
-# from flask_uploads import UploadSet, configure_uploads, IMAGES
+from flask_uploads import UploadSet, configure_uploads, IMAGES
 from flask_simplemde import SimpleMDE
 from flask_mail import Mail
 
 bootstrap = Bootstrap()
 db = SQLAlchemy()
-# photos = UploadSet('photos', IMAGES)
+photos = UploadSet('photos', IMAGES)
 simple = SimpleMDE()
 mail = Mail()
 
@@ -40,6 +40,6 @@ def create_app(config_name):
     app.register_blueprint(auth_blueprint, url_prefix='/authenticate')
 
     # Configure UploadSet
-    # configure_uploads(app, photos)
+    configure_uploads(app, photos)
 
     return app
