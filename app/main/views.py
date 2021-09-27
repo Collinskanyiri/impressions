88dd35071478e4fe1d28214cc9487dc7cacbc14b
Collins Kanyiri <collins.kanyiri@student.moringaschool.com>
2021-09-27 23:03:16 +0300

    deployment to heroku

--- app/main/views.py
+++ app/main/views.py
@@ -1,5 +1,5 @@
 from flask import render_template, request, redirect, url_for, abort
-from . import main
+from app import main
 from flask_login import login_required, current_user
 from ..models import User, Pitch, Comment
 from .forms import PitchForm, CommentForm, UpdateProfile
