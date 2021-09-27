58c8135ec13324119e6faeba8de9cd497bea8006
Collins Kanyiri <collins.kanyiri@student.moringaschool.com>
2021-09-27 14:33:27 +0300

    corrections

--- app/main/__init__.py
+++ app/main/__init__.py
@@ -1,23 +1,3 @@
-from flask import Flask
-from flask_bootstrap import bootstrap
-from config import config_options
-from . import views, errors
 from flask import Blueprint
 main = Blueprint('main', __name__)
-
-
-
-def create_app(config_name):
-    app = Flask(__name__)
-
-    # Creating the app configurations
-    app.config.from_object(config_options[config_name])
-
-    # Initializing flask extensions
-    bootstrap.init_app(app)
-
-    # Registering the blueprint
-    from .main import main as main_blueprint
-    app.register_blueprint(main_blueprint)
-
-    return app
+from . import views, forms
