067f2eeae23a65c3077fe33c84bb4ef392e5f7f6
Collins Kanyiri <collins.kanyiri@student.moringaschool.com>
2021-09-29 15:50:49 +0300

    corrections app/auth/views.py

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,11 +1,7 @@
-from flask import render_template
 from . import auth
-from flask import render_template, redirect, url_for
 from ..models import User
 from .. import db
 from flask import render_template, redirect, url_for, flash, request
-from flask_login import login_user
-from ..models import User
 from .forms import LoginForm, RegistrationForm
 from flask_login import login_user, logout_user, login_required
 from ..email import mail_message
@@ -41,15 +37,16 @@ def login():
         if user is not None and user.verify_password(login_form.password.data):
             login_user(user, login_form.remember.data)
             return redirect(request.args.get('next') or url_for('main.index'))
-
+            
         flash('Invalid username or Password')
 
     title = "impressions login"
     return render_template('auth/login.html', login_form=login_form, title=title)
 
 
-@auth.route('/logout')
+@auth.route("/logout")
 @login_required
 def logout():
     logout_user()
     return redirect(url_for("main.index"))
+
